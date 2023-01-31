111354144b282c1755fc0d117722bf75fa77041d
AmetJunior <ametsejdaliev@gmail.com>
2023-01-31 23:37:33 +0300

    31.01.2023-commit-successfuly-json1

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import requests
 import json
+import numpy as np
+import requests
 
-response = requests.get("https://jsonplaceholder.typicode.com/users")
-todos = json.loads(response.text)
+# response = requests.get("https://jsonplaceholder.typicode.com/users")
+# todos = json.loads(response.text)
+#
+# for entry in todos:
+#     if (entry['id'] == 1):
+#         entry['name'] = 'Java'
+#     print(entry['name'])
+#
+# print(json.dumps(todos))
 
-for entry in todos:
-    if (entry['id'] == 1):
-        entry['name'] = 'Java'
-    print(entry['name'])
+a = ["Hek", "25", "hER"]
+B = []
+for elements in a:
+    index = a.index(elements)
+    if(index == 0):
+        index = 'name'
+    if(index == 1):
+        index = 'street'
+    if(index == 2):
+        index = 'city'
+    elements = f"'{index}': {elements}"
+    B.append(elements)
+    # print(elements)
+arr = np.array(B)
+list1 = arr.tolist()
+with open('test.json', 'w') as f:
+    json.dump(list1, f)
+#
 
-print(json.dumps(todos))
+
+
