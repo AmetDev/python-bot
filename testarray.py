236379baeabe31855ae664f70bee3f6bc48adfa3
AmetJunior <ametsejdaliev@gmail.com>
2023-02-05 19:03:43 +0300

    last-version

--- testarray.py
+++ testarray.py
@@ -1,9 +1,13 @@
 import json
+import keyboard
+
+a = True
+free_space = int()
 
 with open('test.json', 'r') as f:
     arr = json.load(f)
 
-print("Enter your unique id:")
+print("Введите уникальный идентификатор:")
 user = int(input())
 
 user_found = False
@@ -14,11 +18,27 @@ for elements in arr:
 
 if not user_found:
     new_element = {"id": user}
-    new_element["nameparkovka"] = input("Enter the name of the parking lot:")
-    new_element["password"] = input("Enter the password:")
-    new_element["priceparkovka"] = int(input("Enter the price of the parking lot:"))
-    new_element["free_forinvalid"] = input("Is it free for invalid (yes/no):").lower() == "yes"
+    new_element["nameparkovka"] = input("Введите название парковки:")
+    new_element["password"] = input("Введите пароль:")
+    new_element["priceparkovka"] = int(input("Введите цену парковки:"))
+    new_element["free_forinvalid"] = input("Бесплатно для льготников (yes/no):").lower() == "yes"
+    new_element["number of free parking spaces"] = input("Введите количество свободных мест:")
     arr.append(new_element)
+else:
+    for elements in arr:
+        if elements.get('id') == user:
+            print('Добро пожаловать!')
+            print('Чтобы обновить количество свободных мест, нажмите пробел')
+            print('Для выхода из программы нажмите N')
+
+            while a == True:
+                if keyboard.is_pressed('space'):
+                    print("Количество свободных мест изменилось? Указать:")
+                    free_space = int(input())
+                    elements["number of free parking spaces"] = free_space
+                    print("Обновлено количество бесплатных парковочных мест!")
+                if keyboard.is_pressed('n'):
+                    a = False
 
 with open('test.json', 'w') as f:
     json.dump(arr, f, indent=4)
