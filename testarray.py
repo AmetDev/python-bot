3457dc10c2101c824cc31704841c68909dba2ef2
AmetJunior <ametsejdaliev@gmail.com>
2023-02-10 01:49:12 +0300

    last-version-english

--- testarray.py
+++ testarray.py
@@ -4,49 +4,55 @@ import requests
 
 BASE_URL = "http://localhost:5000/posts"
 response = requests.get(BASE_URL)
-response = response.json()
+response_json = response.json()
 
-a = True
-free_space = int()
+running = True
+free_space = 0
 
-print("Введите уникальный идентификатор и запомните его. При последующим входе введете его:")
-user = int(input())
+print("Enter a unique identifier and remember it. On subsequent entry, enter it:")
+user_id = int(input())
 
 user_found = False
-for elements in response:
+for element in response_json:
     
-    if elements.get('owner_id') == user:
+    if element.get('owner_id') == user_id:
         user_found = True
         break
 
 if not user_found:
-    new_element = {"owner_id": user}
-    new_element["owner_name_parking"] = input("Введите название парковки:")
-    new_element["owner_password"] = int(input("Введите пин-код:"))
-    new_element["owner_price_parking"] = int(input("Введите цену парковки:"))
-    new_element["owner_free_forinvalid"] = input("Бесплатно для льготников (yes/no):").lower() == "yes"
-    new_element["owner_number_parking_spaces"] = int(input("Введите количество свободных мест:"))
+    new_element = {"owner_id": user_id}
+    new_element["owner_name_parking"] = input("Enter the name of the parking lot:")
+    new_element["owner_password"] = int(input("Enter the PIN code:"))
+    new_element["owner_price_parking"] = int(input("Enter the price of parking:"))
+    new_element["owner_free_forinvalid"] = input("Free for the handicapped (yes/no):").lower() == "yes"
+    new_element["owner_number_parking_spaces"] = int(input("Enter the number of free spaces:"))
     new_element["User_number_of_free_place_parking"] = new_element["owner_number_parking_spaces"]
     new_element["User_number_of_occupied_parking_spaces"] = 0
-    response = requests.post(BASE_URL, json = new_element)
+    response = requests.post(BASE_URL, data=json.dumps(new_element), headers={'Content-Type': 'application/json'})
 
 else:
-    for elements in response:
-        if elements.get('owner_id') == user:
-            print('Добро пожаловать!')
-            print("Введите ваш пин-код, если уже зарегистрированы!")
+    for element in response_json:
+        if element.get('owner_id') == user_id:
+            print('Welcome!')
+            print("Enter your PIN code if already registered!")
             password = int(input())
-            if elements.get("owner_password") == password:
-                print('Чтобы обновить количество свободных мест, нажмите пробел')
-                print('Для выхода из программы нажмите N')
-                while a == True:
+            if element.get("owner_password") == password:
+                print('To update the number of free spaces, press space')
+                print('To exit the program, press N')
+                while running == True:
                     if keyboard.is_pressed('space'):
-                        print("Количество свободных мест изменилось? Указать:")
+                        print("Has the number of free spaces changed? Indicate:")
                         free_space = int(input())
-                        elements["owner_number_parking_spaces"] = free_space
-                        elements["User_number_of_free_place_parking"] = free_space
-                        print("Обновлено количество бесплатных парковочных мест!")
-                    if keyboard.is_pressed('n'):
-                        a = False
+                        element["owner_number_parking_spaces"] = free_space
+                        element["User_number_of_free_place_parking"] = free_space
     
+                        update_url = BASE_URL + "/" + str(element["owner_id"])
+                        response = requests.put(update_url, data=json.dumps(element), headers={'Content-Type': 'application/json'})
+                        if response.status_code == 200:
+                            print("The number of free parking spaces has been updated!")
+                        else:
+                            print("Failed to update the number of free parking spaces")
 
+                   
+                    if keyboard.is_pressed('n'):
+                        running = False
